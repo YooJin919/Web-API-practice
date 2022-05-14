5bbaa72393d0b8cce01c397c8d53a2ae779cf7a3
knamuuu <knamuuu@naver.com>
2022-05-15 01:36:20 +0900

    modify: prototype

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const server = app.listen(8080, () => {
 const db = require("database/data");
 db.connect();
 
+var getprofile = async function (req, res) {
+  const nickname = req.params;
+
+  try {
+    const [profile] = await db.promise();
+    res.json(profile);
+  } catch {
+    console.log("getprofile got error");
+    res.status(400).json({ text: "ErrorCode:400, Bad Request" });
+  }
+};
+
 var getmatches = async function (req, res) {
   const nickname = req.params;
 
@@ -20,16 +32,18 @@ var getmatches = async function (req, res) {
   }
 };
 
-var getdetail = async function (req, res) {
+var getmatchdetail = async function (req, res) {
   const nickname = req.params;
 
   try {
     const [matches] = await db.promise();
     res.json(matches);
   } catch {
-    console.log("getdetail got error");
+    console.log("getmatchdetail got error");
     res.status(400).json({ text: "ErrorCode:400, Bad Request" });
   }
 };
 
+app.get("/profile/:nickname", getprofile);
 app.get("/matches/:nickname", getmatches);
+app.get("/matches/detail/:nickname", getmatchdetail);
