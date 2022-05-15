2debcc8e0dc673029437b173863a520d71ce150d
YooJin919 <dbwls30131@naver.com>
2022-05-15 18:56:29 +0900

    18:56

--- server/server.js
+++ server/server.js
@@ -1,33 +1,50 @@
 // Create Web Server
 // mysql 패키지 
-const mysql = require('mysql');
+const mysql = require("mysql2");
 // axios는 http 요청 관리하는 패키지
 //const axios = require('axios');
 var http = require('http');
 const express = require("express");
+// const sequelize = require('./models/index').sequelize;
 const port = 3000
+// sequelize.sync();
 // .env 파일을 읽어오기 위한 패키지
 require('dotenv').config();
 
 app=express();
 
+// front로 보낼 객체
 let user = {
     nickname : '',
-    puuid : 0,
+    levels : 0,
+    icon_id : 0,
 }
+let rank_tier = {
+    tier : '',
+    sub_tier : '',
+    league_point : 0,
+    date : Date,
+    win : 0,
+    defeat : 0,
+    top4 : 0,
+}
+let superfast_tier = {
+    sf_date : Date,
+    sf_tier : '',
+    sf_league_point :0,
+}
+let user_puuid = '';
 
 // DB 코드
-// createConnection method를 사용하고 인자로 객체를 줌
-let connection = mysql.createConnection({
+const db = mysql.createConnection({ // createConnection method를 사용하고 인자로 객체를 줌
     host: process.env.MYSQL_HOST,
     user: "root",
     port: "3306",
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    
 });
-connection.connect(); // connect method 호출하면 접속됨
 console.log("mysql connection success");
-
 
 
 // -------------------------------------------------------------------- //
@@ -37,24 +54,44 @@ app.get('/', function(req, res){
 	return res.send(req.url);
 })
 
-app.get("/summoners/by-name/:nickname", async function(req, res){
-    let { nickname } = req.params;
-    console.log('req.params : '+nickname);
-    res.send(nickname)
-    // 1. 받은 nickname으로 DB에서 puuid 찾기
-    // query메소드에서 첫번째 인자로 sql쿼리문을 주고, 두번째 인자로 callback을 줌
-    // callback함수의 첫번째 인자로 error, 두번째 인자로 접속 결과를 줌
-    connection.query('SELECT puuid FROM user WHERE user.nickname=nickname', function(err, results){
-        if(err){
-            console.log(error);
-        }
-            console.log(results); // topic의 데이터가 객체형태로 반환
+app.get("/search/:nickname", async function(req, res){
+    
+}
 
-        user.nickname = nickname;
-        user.puuid = results;
-    }); 
-	return res.send(user_nickname);
-});
+
+
+
+
+// app.get("/search/:nickname", async function(req, res){
+//     try {
+//         // 1. 받은 nickname으로 DB에서 puuid 찾기
+//         // query메소드에서 첫번째 인자로 sql쿼리문을 주고, 두번째 인자로 callback을 줌
+//         // callback함수의 첫번째 인자로 error, 두번째 인자로 접속 결과를 줌
+//         // User Info
+//         let {nickname} = req.params;
+//         let sql = `SELECT * FROM user WHERE nickname='${nickname}'`;
+//         const re = await db.query(sql, function(err, results){
+//             user_puuid = results[0].puuid;
+//             console.log(data);
+//             user = {
+//                 nickname : results[0].nickname,
+//                 levels : results[0].levels
+//             }
+//             if(err){
+//                 console.log(err);
+//             }
+//             else{
+//                 console.log(re);
+//                 //console.log('user_puuid : ', user_puuid, ' user : ', user); // topic의 데이터가 객체형태로 반환
+//             }
+//         }); 
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+
+// 	return res.send(req.params);
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
